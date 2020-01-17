1539727c514cfd17eb7fcdc5682ea553fc74e0e3
nanqing <Nan159357>
2020-01-17 14:45:02 +0800

    bubbleSort

--- src/main/java/cn/leetcode/algorithm/array/lesson1.java
+++ src/main/java/cn/leetcode/algorithm/array/lesson1.java
@@ -1,9 +1,36 @@
 package cn.leetcode.algorithm.array;
 
+import java.util.Arrays;
+
 /**
- *  Mininum Size Subarray Sum
+ *  冒泡排序
  */
 public class lesson1 {
+
+    private static int[] bubbleSort(int[] array){
+        int len=array.length;
+        int temp;
+        for(int i=0;i<len-1;i++){
+            for(int j=0;j<len-1-i;j++){
+                if(array[j]<array[j+1]){
+                    temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array={2,8,4,6,3,7};
+        bubbleSort(array);
+        for (int i : array) {
+            System.out.print(i+" ");
+        }
+
+    }
 
 
 }
